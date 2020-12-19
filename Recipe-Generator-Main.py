f02742f2995d0cec6efc1fa93f2df1c0bd55c831
Rahul Arepaka <rahul.arepaka@gmail.com>
2020-12-19 11:06:04 +0400

    Update Recipe-Generator-Main.py

--- Recipe-Generator-Main.py
+++ Recipe-Generator-Main.py
@@ -1,9 +1,10 @@
+# Import All Libraries
 from urllib.request import urlopen
 import urllib
 import json
 from datetime import datetime
 
-
+# API Intialisation
 def recipe_search(ingredient):
     api_key = '7a7cd9b85dd6e0d516903b7c2d582bfa'
     application_id = 'a720f909'
@@ -12,22 +13,30 @@ def recipe_search(ingredient):
     data = json.load(result)
     return data['hits']
 
-
+# User Inputs the ingredient
 ingredient = input('Enter an ingredient: ')
 
+# Renaming the File to the ingredient 
 opname = ingredient + '-recipes.txt'
 
+# File Location 
 outFileName = "D:\\Python - Projects\\food_reciepe\\" + str(opname)
+
+#Creating the File with Encoding
 report = open(outFileName, "w", encoding="utf-8")
 
+# Date and Time
 now = datetime.now()
 dt = now.strftime("%d/%m/%Y %H:%M:%S")
 
+# API Call
 hits = recipe_search(ingredient)
 
+# Heading the Text File
 report.write("--------------------------------------------Recipe Generated From " +ingredient+"---"+dt+"--------------------------" + '\n')
 report.write('\n')
 
+# Loop for Searching Recpies
 for single_hit in hits:
     recipe_json = single_hit['recipe']
     report.write(str(recipe_json['label']) + '\n')
@@ -38,5 +47,5 @@ for single_hit in hits:
 
 print('Please Find the Text File Saved in this directory')
 
-
+#Closing the File Created
 report.close()
